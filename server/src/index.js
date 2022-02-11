be0c9772dc499889870f9e501bc9c1e6d7fe3735
Jacob Lee <jacobleecs@gmail.com>
2022-02-11 16:07:54 -0500

    Initial setup for BFF

---
+++ server/src/index.js
@@ -0,0 +1,12 @@
+const { ApolloServer } = require('apollo-server')
+const typeDefs = require('./schema')
+const resolvers = require('./resolvers')
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen().then(() => {
+    console.log('ApolloServer is running!')
+})
